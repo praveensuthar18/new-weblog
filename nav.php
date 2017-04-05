38ab5cdd247128dfcd8a104b8d93b11d77cc6def
Praveen Kumar Suthar <kalamsuthar@gmail.com>
2017-04-06 02:09:21 +0530

    Update nav.php

--- nav.php
+++ nav.php
@@ -3,26 +3,26 @@
 		<ul class="sidebar-nav">
             <li>
 				<a href="post.php" title="New Post">
-					<span class="fa fa-anchor solo">New Post</span>
+					<span class="fa fa-book ">New Post</span>
 				</a>
 			</li>
             <li>
 				<a href="search.php" title="Search">
-					<span class="fa fa-anchor solo">Search</span>
+					<span class="fa fa-search">Search</span>
 				</a>
 			</li>
             <li>
 				<a href="index.php" title="Return to Home Page">
 
-					<span class="fa fa-anchor solo">Home Page</span>
+					<span class="fa fa-home">Home Page</span>
 				</a>
 			</li>
           <?php
            if(isset($_SESSION['user_id'])){
-               echo '<li><a href="members-page.php" title="Member\'s Page">My Page</a></li>';
+               echo '<li><a href="members-page.php" title="Member\'s Page"><i class= "fa fa-user-o "></i>My Page</a></li>';
             }
            if(isset($_SESSION['user_level']) && ($_SESSION['user_level'] == 2)){
-               echo '<li><a href="admin-page.php" title="Admin Page">Admin Page</a></li>';
+               echo '<li><a href="admin-page.php" title="Admin Page"><i class="fa fa-users"></i>Admin Page</a></li>';
             }
           ?>
         </ul>
